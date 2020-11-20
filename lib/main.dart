402169db06e703f6425e169353d2a84a4519dd97
zzafergok <41114328+zzafergok@users.noreply.github.com>
2020-11-20 23:21:11 +0300

    create home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pages/pokemon_list.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,109 +10,319 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Project Stark',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+        primarySwatch: Colors.deepOrange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("Main Menu"),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Card(
+                  //color: Colors.lightBlue,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PokemonList()));
+                    },
+                    child: Container(
+                        height: 275,
+                        width: 316,
+                        margin: EdgeInsets.only(top: 35),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Image.network(
+                              "https://compote.slate.com/images/18ba92e4-e39b-44a3-af3b-88f735703fa7.png?width=780&height=520&rect=1560x1040&offset=0x0",
+                              fit: BoxFit.scaleDown,
+                            ),
+                            Chip(label: Text("Pokemon"),backgroundColor: Colors.amberAccent,)
+                          ],
+                        )),
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24)),
+                  elevation: 6,
+                  margin: EdgeInsets.all(16),
+                ),
+                Card(
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PokemonList()));
+                    },
+                    child: Container(
+                        height: 275,
+                        width: 316,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Image.network(
+                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPwnWCx7yYJFgU8BIfXzpkgV3NwwIWuwGC9w&usqp=CAU",
+                              fit: BoxFit.fitWidth,scale: 0.9,
+                            ),
+                            Chip(label: Text("Final Space", style: TextStyle(color: Colors.white),),backgroundColor: Colors.black45,)
+                          ],
+                        )),
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24)),
+                  elevation: 6,
+                  margin: EdgeInsets.all(16),
+                ),
+                Card(
+                  //color: Colors.lightBlue,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PokemonList()));
+                    },
+                    child: Container(
+                        height: 225,
+                        width: 316,
+                        margin: EdgeInsets.only(top: 45),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Image.network(
+                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZB6ELN-_hxUSoDL6ke9xgcJpb5PmNw65_Rg&usqp=CAU",
+                              fit: BoxFit.cover, scale: 0.95
+                            ),
+                            Chip(label: Text("Rick and Mort", style: TextStyle(color: Colors.white),),backgroundColor: Colors.purpleAccent,)
+                          ],
+                        )),
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24)),
+                  elevation: 6,
+                  margin: EdgeInsets.all(16),
+                ),
+                Card(
+                  //color: Colors.lightBlue,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PokemonList()));
+                    },
+                    child: Container(
+                        height: 225,
+                        width: 316,
+                        margin: EdgeInsets.only(top: 15),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Image.network(
+                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0HWYsi4GitQVALi7hE5JOKUYsbCpGuBBjbw&usqp=CAU",
+                            ),
+                            Chip(label: Text("Brooklyn Nine Nine"),backgroundColor: Colors.amberAccent,)
+                          ],
+                        )),
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24)),
+                  elevation: 6,
+                  margin: EdgeInsets.all(16),
+                ),
+                Card(
+                  //color: Colors.lightBlue,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PokemonList()));
+                    },
+                    child: Container(
+                        height: 225,
+                        width: 316,
+                        margin: EdgeInsets.only(top: 45),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Image.network(
+                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLfuLJQ7DIZAOHyF_WYVtj3ke-sx7TQwHQHw&usqp=CAU",
+                              scale: 0.9,
+                            ),
+                            Chip(label: Text("Family Guy"),backgroundColor: Colors.orange,)
+                          ],
+                        )),
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24)),
+                  elevation: 6,
+                  margin: EdgeInsets.all(16),
+                ),
+                Card(
+                  //color: Colors.lightBlue,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PokemonList()));
+                    },
+                    child: Container(
+                        //color: Colors.grey,
+                        height: 225,
+                        width: 316,
+                        margin: EdgeInsets.only(top: 35),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Image.network(
+                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvriuYh0t7piD671xrgKtH954CTZMbuTWZ4A&usqp=CAU",
+                              scale: 0.9,
+                            ),
+                            Chip(label: Text("Game of Thrones"),backgroundColor: Colors.black26,)
+                          ],
+                        )),
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24)),
+                  elevation: 6,
+                  margin: EdgeInsets.all(16),
+                ),
+                Card(
+                  //color: Colors.lightBlue,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PokemonList()));
+                    },
+                    child: Container(
+                        height: 225,
+                        width: 316,
+                        margin: EdgeInsets.only(top: 45),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Image.network(
+                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQf4jSrvrGVs3ZANoaSYumE_BCRF2aehYXDYA&usqp=CAU",
+                                scale: 0.9,
+                            ),
+                            Chip(label: Text("Modern Family", style: TextStyle(color: Colors.white70),),backgroundColor: Colors.blueGrey,)
+                          ],
+                        )),
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24)),
+                  elevation: 6,
+                  margin: EdgeInsets.all(16),
+                ),
+                Card(
+                  //color: Colors.lightBlue,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PokemonList()));
+                    },
+                    child: Container(
+                        height: 225,
+                        width: 316,
+                        margin: EdgeInsets.only(top: 40),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Image.network(
+                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo-eQrmv0uN2mRdI2HHmUpPKb_ST8T1eizrw&usqp=CAU",
+                                scale: 0.95,
+                            ),
+                            Chip(label: Text("Supernatural", style: TextStyle(color: Colors.white),),backgroundColor: Colors.black54,)
+                          ],
+                        )),
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24)),
+                  elevation: 6,
+                  margin: EdgeInsets.all(16),
+                ),
+                Card(
+                  //color: Colors.lightBlue,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PokemonList()));
+                    },
+                    child: Container(
+                        height: 225,
+                        width: 316,
+                        margin: EdgeInsets.only(top: 45),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Image.network(
+                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnMznyP1NgjxPEPALPLAgj-_CuOvZXGfGoCQ&usqp=CAU",
+                              scale: 0.95,
+                            ),
+                            Chip(label: Text("The Big Bang Theory"),backgroundColor: Colors.orange,)
+                          ],
+                        )),
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24)),
+                  elevation: 6,
+                  margin: EdgeInsets.all(16),
+                ),
+                Card(
+                  //color: Colors.lightBlue,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PokemonList()));
+                    },
+                    child: Container(
+                        height: 225,
+                        width: 316,
+                        margin: EdgeInsets.only(top: 45),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Image.network(
+                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqUCzXwCT9G1kvEw-C-TUemRidOPrJ0n7Z8A&usqp=CAU",
+                              scale: 0.8,
+                            ),
+                            Chip(label: Text("The Simpsons"),backgroundColor: Colors.amberAccent,)
+                          ],
+                        )),
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24)),
+                  elevation: 6,
+                  margin: EdgeInsets.all(16),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
