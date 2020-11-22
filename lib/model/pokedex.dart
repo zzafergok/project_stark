91cedc319637140e0c6026779354e7a08548709a
zzafergok <41114328+zzafergok@users.noreply.github.com>
2020-11-23 02:11:34 +0300

    json file updated

--- lib/model/pokedex.dart
+++ lib/model/pokedex.dart
@@ -16,12 +16,13 @@ class Pokedex {
   List<Pokemon> pokemon;
 
   factory Pokedex.fromJson(Map<String, dynamic> json) => Pokedex(
-    pokemon: List<Pokemon>.from(json["pokemon"].map((x) => Pokemon.fromJson(x))),
-  );
+        pokemon:
+            List<Pokemon>.from(json["pokemon"].map((x) => Pokemon.fromJson(x))),
+      );
 
   Map<String, dynamic> toJson() => {
-    "pokemon": List<dynamic>.from(pokemon.map((x) => x.toJson())),
-  };
+        "pokemon": List<dynamic>.from(pokemon.map((x) => x.toJson())),
+      };
 }
 
 class Pokemon {
@@ -64,44 +65,58 @@ class Pokemon {
   List<Evolution> prevEvolution;
 
   factory Pokemon.fromJson(Map<String, dynamic> json) => Pokemon(
-    id: json["id"],
-    num: json["num"],
-    name: json["name"],
-    img: json["img"],
-    type: List<String>.from(json["type"].map((x) => x)),
-    height: json["height"],
-    weight: json["weight"],
-    candy: json["candy"],
-    candyCount: json["candy_count"] == null ? null : json["candy_count"],
-    egg: json["egg"],
-    spawnChance: json["spawn_chance"].toDouble(),
-    avgSpawns: json["avg_spawns"].toDouble(),
-    spawnTime: json["spawn_time"],
-    multipliers: json["multipliers"] == null ? null : List<double>.from(json["multipliers"].map((x) => x.toDouble())),
-    weaknesses: List<String>.from(json["weaknesses"].map((x) => x)),
-    nextEvolution: json["next_evolution"] == null ? null : List<Evolution>.from(json["next_evolution"].map((x) => Evolution.fromJson(x))),
-    prevEvolution: json["prev_evolution"] == null ? null : List<Evolution>.from(json["prev_evolution"].map((x) => Evolution.fromJson(x))),
-  );
+        id: json["id"],
+        num: json["num"],
+        name: json["name"],
+        img: json["img"],
+        type: List<String>.from(json["type"].map((x) => x)),
+        height: json["height"],
+        weight: json["weight"],
+        candy: json["candy"],
+        candyCount: json["candy_count"] == null ? null : json["candy_count"],
+        egg: json["egg"],
+        spawnChance: json["spawn_chance"].toDouble(),
+        avgSpawns: json["avg_spawns"].toDouble(),
+        spawnTime: json["spawn_time"],
+        multipliers: json["multipliers"] == null
+            ? null
+            : List<double>.from(json["multipliers"].map((x) => x.toDouble())),
+        weaknesses: List<String>.from(json["weaknesses"].map((x) => x)),
+        nextEvolution: json["next_evolution"] == null
+            ? null
+            : List<Evolution>.from(
+                json["next_evolution"].map((x) => Evolution.fromJson(x))),
+        prevEvolution: json["prev_evolution"] == null
+            ? null
+            : List<Evolution>.from(
+                json["prev_evolution"].map((x) => Evolution.fromJson(x))),
+      );
 
   Map<String, dynamic> toJson() => {
-    "id": id,
-    "num": num,
-    "name": name,
-    "img": img,
-    "type": List<dynamic>.from(type.map((x) => x)),
-    "height": height,
-    "weight": weight,
-    "candy": candy,
-    "candy_count": candyCount == null ? null : candyCount,
-    "egg": egg,
-    "spawn_chance": spawnChance,
-    "avg_spawns": avgSpawns,
-    "spawn_time": spawnTime,
-    "multipliers": multipliers == null ? null : List<dynamic>.from(multipliers.map((x) => x)),
-    "weaknesses": List<dynamic>.from(weaknesses.map((x) => x)),
-    "next_evolution": nextEvolution == null ? null : List<dynamic>.from(nextEvolution.map((x) => x.toJson())),
-    "prev_evolution": prevEvolution == null ? null : List<dynamic>.from(prevEvolution.map((x) => x.toJson())),
-  };
+        "id": id,
+        "num": num,
+        "name": name,
+        "img": img,
+        "type": List<dynamic>.from(type.map((x) => x)),
+        "height": height,
+        "weight": weight,
+        "candy": candy,
+        "candy_count": candyCount == null ? null : candyCount,
+        "egg": egg,
+        "spawn_chance": spawnChance,
+        "avg_spawns": avgSpawns,
+        "spawn_time": spawnTime,
+        "multipliers": multipliers == null
+            ? null
+            : List<dynamic>.from(multipliers.map((x) => x)),
+        "weaknesses": List<dynamic>.from(weaknesses.map((x) => x)),
+        "next_evolution": nextEvolution == null
+            ? null
+            : List<dynamic>.from(nextEvolution.map((x) => x.toJson())),
+        "prev_evolution": prevEvolution == null
+            ? null
+            : List<dynamic>.from(prevEvolution.map((x) => x.toJson())),
+      };
 }
 
 class Evolution {
@@ -114,12 +129,12 @@ class Evolution {
   String name;
 
   factory Evolution.fromJson(Map<String, dynamic> json) => Evolution(
-    num: json["num"],
-    name: json["name"],
-  );
+        num: json["num"],
+        name: json["name"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "num": num,
-    "name": name,
-  };
+        "num": num,
+        "name": name,
+      };
 }
