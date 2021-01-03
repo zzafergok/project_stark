90cf776197b26251313f1ad9888e93059bb9e873
zzafergok <41114328+zzafergok@users.noreply.github.com>
2021-01-03 14:28:55 +0300

    a simple structure has been passed

--- lib/pages/brooklyn_nine_nine_cast_list.dart
+++ lib/pages/brooklyn_nine_nine_cast_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:project_stark/services/brooklyn_nine_nine_services.dart';
 import 'package:project_stark/model/brooklyn_nine_nine_cast.dart';
+import 'package:flip_card/flip_card.dart';
 
 class BrooklynNineNineCastList extends StatefulWidget {
   BrooklynNineNineCastList() : super();
@@ -38,92 +39,43 @@ class _BrooklynNineNineCastListState extends State<BrooklynNineNineCastList> {
         child: ListView.builder(
           itemCount:
               null == _brooklynNineNineCast ? 0 : _brooklynNineNineCast.length,
-          itemExtent: 300,
+          itemExtent: 390,
           itemBuilder: (context, index) {
             BrooklynNineNineCast brooklynNineNineCast =
                 _brooklynNineNineCast[index];
             return Container(
               color: Colors.blueGrey,
               child: Card(
-                elevation: 6,
-                margin: EdgeInsets.all(12),
+                margin: EdgeInsets.all(20),
                 color: Colors.amber,
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: Container(
-                        margin: EdgeInsets.only(right: 8, top: 50, bottom: 50),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            Container(
-                              height: 20,
-                              width: 150,
-                              child: Center(
-                                  child: Text(
-                                      brooklynNineNineCast.character.name)),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(16),
-                                  color: Colors.orangeAccent),
-                            ),
-                            Container(
-                              height: 20,
-                              width: 150,
-                              child: Center(
-                                  child:
-                                      Text(brooklynNineNineCast.person.name)),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(16),
-                                  color: Colors.tealAccent),
-                            ),
-                            Container(
-                              height: 20,
-                              width: 150,
-                              child: Center(
-                                  child: Text(brooklynNineNineCast
-                                      .person.country.name
-                                      .toString())),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(16),
-                                  color: Colors.tealAccent),
-                            ),
-                            Container(
-                              padding: EdgeInsets.only(left: 35, top: 1.5),
-                              height: 20,
-                              width: 150,
-                              child: Center(
-                                  child: Text(
-                                brooklynNineNineCast.person.birthday.toString(),
-                              )),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(16),
-                                  color: Colors.tealAccent),
-                            ),
-                          ],
-                        ),
+                child: FlipCard(
+                  direction: FlipDirection.HORIZONTAL,
+                  speed: 500,
+                  flipOnTouch: true,
+                  front: Container(
+                    margin: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            brooklynNineNineCast.character.image.original),
                       ),
                     ),
-                    Expanded(
-                      child: Container(
-                        margin: EdgeInsets.only(right: 8),
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: NetworkImage(
-                                brooklynNineNineCast.character.image.original),
-                          ),
-                        ),
+                  ),
+                  back: Container(
+                    margin: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            brooklynNineNineCast.character.image.original),
                       ),
                     ),
-                  ],
+                  ),
                 ),
                 shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16)),
+                    borderRadius: BorderRadius.circular(24)),
               ),
             );
           },
-          //itemCount: _brooklynNineNineCast.length,
         ),
       ),
     );
